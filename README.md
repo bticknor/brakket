03a67a5ee456e73f15b5b715805c87af21c66f37
Benjamin Ticknor <ticknorbenjamin@gmail.com>
2020-02-15 14:22:26 -0500

    add the stuffs, including lots of extra akka stuff

---
+++ README.md
@@ -0,0 +1,6 @@
+- each game will be an actor
+
+- championship game will be guardianactor? recursively spawn additional actors on startup of the app?
+
+- message handled will be a case class Winner(whom: Team) 
+ 
---
+++ src/main/scala/com/example/Game.scala
@@ -0,0 +1,53 @@
+//#full-example
+package com.example
+
+import akka.actor.Actor
+
+import akka.actor.typed.ActorRef
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import com.example.GreeterMain.SayHello
+
+
+case class Team(name: String, seed: Int)
+
+
+object GameSimulation {
+
+  // TODO simulate the result of this
+  def simulateGameResult(team1: Team, team2: Team) = team1
+
+}
+
+
+class BracketGame extends Actor {
+
+  case class Winner(team: Team)
+
+  def receive = active(Set.empty[Team])
+
+  // our internal state is the set of winning teams we've received from prior games 
+  def active(teamsReceived: Set[Team]): Receive = {
+    // if we have already received the other winner, simulate the game with the
+    // received team
+    case Winner(team) => {
+      if(teamsReceived.size == 0) {
+        // if we haven't received the other winner yet
+        // we add the winner we've received into the teams buffer
+        context become active(teamsReceived + team)
+      } else {
+        // if we have received the other winner, simulate that team vs. the received team
+        val winner = GameSimulation.simulateGameResult(
+          teamsReceived.head, team
+        )
+        // then send that winner as a message to the next game
+        // TODO: add current team to set of teams used in simulation
+      }
+    }
+     
+  }
+}
+
+
+
