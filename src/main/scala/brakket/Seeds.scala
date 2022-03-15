5a7755028947b602502c4f434b1d983c3bdbbe63
Benjamin Ticknor <ticknorbenjamin@gmail.com>
2022-03-14 20:23:44 -0500

    put seeds into scala source file instead of json resources

---
+++ src/main/scala/brakket/Seeds.scala
@@ -0,0 +1,82 @@
+package brakket
+
+object Seeds {
+  // teams for every seed in every region
+  // manually update this file every year
+
+  val eastRegion = Map(
+    "1" -> "Baylor",
+    "2" -> "Kentucky",
+    "3" -> "Purdue",
+    "4" -> "UCLA",
+    "5" -> "Saint Mary's",
+    "6" -> "Texas",
+    "7" -> "Murray St.",
+    "8" -> "UNC",
+    "9" -> "Marquette",
+    "10" -> "San Francisco",
+    "11" -> "Virginia Tech",
+    "12" -> "WYO/IND",
+    "13" -> "Akron",
+    "14" -> "Yale",
+    "15" -> "St. Peter's",
+    "16" -> "Norfolk St."
+  )
+
+  val southRegion = Map(
+    "1" -> "Arizona",
+    "2" -> "Villanova",
+    "3" -> "Tennessee",
+    "4" -> "Illinois",
+    "5" -> "Houston",
+    "6" -> "Colorado St.",
+    "7" -> "Ohio St.",
+    "8" -> "Seton Hall",
+    "9" -> "TCU",
+    "10" -> "Loyola Chicago",
+    "11" -> "Michigan",
+    "12" -> "UAB",
+    "13" -> "Chattanooga",
+    "14" -> "Longwood",
+    "15" -> "Delaware",
+    "16" -> "WRST/BRY"
+  )
+
+  val westRegion = Map(
+    "1" -> "Gonzaga",
+    "2" -> "Duke",
+    "3" -> "Texas Tech",
+    "4" -> "Arkansas",
+    "5" -> "Connecticut",
+    "6" -> "Alabama",
+    "7" -> "Michigan St.",
+    "8" -> "Boise St.",
+    "9" -> "Memphis",
+    "10" -> "Davidson",
+    "11" -> "RUTG/ND",
+    "12" -> "New Mexico St.",
+    "13" -> "Vermont",
+    "14" -> "Montana St.",
+    "15" -> "CS Fullerton",
+    "16" -> "Georgia St."
+  )
+
+  val midwestRegion = Map(
+    "1" -> "Kansas",
+    "2" -> "Auburn",
+    "3" -> "Wisconsin",
+    "4" -> "Providence",
+    "5" -> "Iowa",
+    "6" -> "LSU",
+    "7" -> "USC",
+    "8" -> "San Diego St.",
+    "9" -> "Creighton",
+    "10" -> "Miami",
+    "11" -> "Iowa St.",
+    "12" -> "Richmond",
+    "13" -> "S. Dakota State",
+    "14" -> "Colgate",
+    "15" -> "Jax. State",
+    "16" -> "TXSO/TAMC"
+  )
+}
